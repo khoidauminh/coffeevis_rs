4cf4c4f1f686b971beced7ed073b2d4fda77b428
Khoidauminh <81458575+khoidauminh@users.noreply.github.com>
2022-02-26 17:23:37 +0700

    Update cross.rs

--- src/graphics/visualizers/cross.rs
+++ src/graphics/visualizers/cross.rs
@@ -3,7 +3,7 @@ use crate::constants::{WIN_H, WIN_W};
 
 static mut grid: bool = false;
 
-pub unsafe fn draw_cross(buf : &mut Vec<u32>) {
+pub unsafe fn draw_cross(buf : &mut [u32]) {
     if {grid ^= true; grid} {
         draw_rect(buf, WIN_W/2, 8, 1, WIN_H-16, 0x00_55_55_55);
     } else {
