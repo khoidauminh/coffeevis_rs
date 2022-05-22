bb1009d0e8dab554b57e60850d49cdada496e3fc
Khoidauminh <81458575+khoidauminh@users.noreply.github.com>
2022-05-22 08:41:30 +0700

    Update dash_line.rs

--- src/graphics/visualizers/dash_line.rs
+++ src/graphics/visualizers/dash_line.rs
@@ -1,39 +1,39 @@
 // THis visualzier is not intended to display on its own.
 
-use crate::constants::{PHASE_OFFSET, INCREMENT, VOL_SCL, WAV_WIN, console_clear, WIN_W, WIN_H};
+use crate::constants::{PHASE_OFFSET, INCREMENT};
 use crate::graphics::graphical_fn::{rgb_to_u32, coord_to_1d, win_clear, win_clear_alpha, draw_line, P2, p2_add};
 use crate::graphics::graphical_fn;
 
 static mut _i : usize = 0;
 const c : u32 = 16720064;
 
-pub unsafe fn draw_dash_line(buf : &mut [u32], stream : &[(f32, f32)], horizontal : bool, offset : usize, flip_side : bool) {
+pub fn draw_dash_line(buf : &mut [u32], stream : &[(f32, f32)], horizontal : bool, offset : usize, flip_side : bool, para: &mut crate::constants::Parameters) {
 
     if (horizontal) {
 
         let o = if flip_side {
-            WIN_H - offset -1
+            para.WIN_H - offset -1
         } else {
             offset
         };
 
-        for i in 0..WIN_W {
-            let index = ((_i+i)/10)%stream.len();
-            graphical_fn::draw_rect(buf, i, o, 2, 2, graphical_fn::apply_alpha(c, (stream[index].0 * 32784.0 % 256.0) as i32 as u8));
+        for i in 0..para.WIN_W {
+            let index = ((para._i+i)/10)%stream.len();
+            graphical_fn::draw_rect(buf, i, o, 2, 2, graphical_fn::apply_alpha(c, (stream[index].0 * 32784.0 % 256.0) as i32 as u8), para);
         }
     } else {
 
         let o = if flip_side {
-            WIN_W - offset -1
+            para.WIN_W - offset -1
         } else {
             offset
         };
 
-        for i in 0..WIN_H {
-            let index = ((_i+i)/10)%stream.len();
-            graphical_fn::draw_rect(buf, o, i, 2, 2, graphical_fn::apply_alpha(c, (stream[index].0 * 32784.0 % 256.0) as i32 as u8));
+        for i in 0..para.WIN_H {
+            let index = ((para._i+i)/10)%stream.len();
+            graphical_fn::draw_rect(buf, o, i, 2, 2, graphical_fn::apply_alpha(c, (stream[index].0 * 32784.0 % 256.0) as i32 as u8), para);
         }
     }
 
-    _i = (_i + INCREMENT)%WIN_H;
+    para._i = (para._i + INCREMENT)%para.WIN_H;
 }
