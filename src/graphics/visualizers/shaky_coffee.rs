0de49bba87cd987b1f2b5f645513bff7cb429ed0
Khoidauminh <81458575+khoidauminh@users.noreply.github.com>
2022-05-22 08:46:17 +0700

    Update shaky_coffee.rs

--- src/graphics/visualizers/shaky_coffee.rs
+++ src/graphics/visualizers/shaky_coffee.rs
@@ -1,23 +1,8 @@
-use std::fs::File;
-use std::io::Read;
-use std::path::Path;
-use std::io::{BufRead, BufReader};
-
-use crate::constants::{WIN_W, WIN_H, VOL_SCL};
+use crate::constants::{Parameters, Image};
 use crate::graphics::{graphical_fn};
 use crate::math;
 
-// default image.
-//use crate::assets::coffee_pixart::coffee_pixart_file;
-//use crate::assets::coffee_pixart_map::cp;
-
-static mut w: usize = 128;
-static mut h: usize = 128;
-
-//const img : &str = "./coffee_pixart_2x.ppm";
-
 // soft shaking
-static mut _i : f32 = 0.0;
 const incr : f32 = 0.0001;
 
 fn diamond_func(amp : f32, prd : f32, t : f32) -> (i32, i32) {
@@ -31,14 +16,18 @@ fn triangle_wav(amp : f32, prd : f32, t : f32) -> f32 {
     (4.0*(t/prd - (t/prd+0.5).trunc()).abs()-1.0)*amp
 }
 
-static mut image_buffer : Vec<u8> = Vec::new();
-
-pub unsafe fn prepare_img(file: &[u8]) {
-    //let path = Path::new(path_string);
-
-//    let mut f = File::open(
-//        path_string
-//   ).expect(&format!("Failed to open file: {:?}", path_string));
+pub fn prepare_img(file: &[u8]) -> Image {
+    use std::fs::File;
+	use std::io::Read;
+	use std::path::Path;
+	use std::io::{BufRead, BufReader};
+	
+    let mut img = Image {
+		w: 0,
+		h: 0,
+		image_buffer: Vec::new()
+	};
+    let mut image_buffer: Vec<u8> = Vec::new();
 
     let reader = BufReader::new(file);
 
@@ -48,9 +37,9 @@ pub unsafe fn prepare_img(file: &[u8]) {
         if index == 0 || index == 2 { continue; }
         if index == 1 {
             let split :Vec<&str> = line.split(' ').collect();
-            w = split[0].parse::<usize>().expect("failed to parse string.");
-            h = split[1].parse::<usize>().expect("failed to parse string.");
-
+            img.w = split[0].parse::<usize>().expect("failed to parse string.");
+            img.h = split[1].parse::<usize>().expect("failed to parse string.");
+		
             continue;
         }
 
@@ -58,15 +47,13 @@ pub unsafe fn prepare_img(file: &[u8]) {
         let mut i = 0;
 
         for i in 0..split.len()-1 {
-            image_buffer.push(
+            img.image_buffer.push(
                 split[i].parse::<u8>().expect("failed to parse string.")
             );
         }
     }
 
-    //sprintln!("{} {}", w, h);
-
-    //println!("{}", image_buffer.len());
+	return img;
 }
 /*
 pub unsafe fn prepare_img_default() {
@@ -99,67 +86,79 @@ pub unsafe fn prepare_img_default() {
 }
 */
 
-static mut x_ : i32 = 0;
-static mut y_ : i32 = 0;
+//~ static mut x_ : i32 = 0;
+//~ static mut y_ : i32 = 0;
 
-pub unsafe fn draw_shaky(buf : &mut [u32], stream : &[(f32, f32)]) {
+pub fn draw_shaky(buf : &mut [u32], stream : &[(f32, f32)], para: &mut Parameters ) {
 
-    apply_coord3(stream);
+    apply_coord3(stream, para);
 
-    let center = (WIN_W as i32 / 2 - w as i32 /2, WIN_H as i32 / 2 - h as i32 /2);
-
-    for i in 0..(w*h) {
-        let x = i%w;
-        let y = i/w;
-
-        let (x_soft_shake, y_soft_shake) = diamond_func(8.0, 1.0, _i);
-
-        buf[graphical_fn::coord_to_1d(center.0 + x as i32 + x_ + 6 + x_soft_shake, center.1 + y as i32 + y_ + y_soft_shake)] = 
-            graphical_fn::rgb_to_u32(
-                image_buffer[i*3],
-                image_buffer[i*3+1],
-                image_buffer[i*3+2]
+    let center = (para.WIN_W as i32 / 2 - para.IMG.w as i32 /2, para.WIN_H as i32 / 2 - para.IMG.h as i32 /2);
+    
+    let (x_soft_shake, y_soft_shake) = diamond_func(8.0, 1.0, para.shaky_coffee.0);
+    
+	for cx in 0..para.WIN_W {
+		for cy in 0..para.WIN_H {
+			let ix = cx*para.IMG.w/para.WIN_W;
+			let iy = cy*para.IMG.h/para.WIN_H;
+			
+			let i = graphical_fn::flatten(ix as i32, iy as i32, para.IMG.w, para.IMG.h);
+			let c = graphical_fn::flatten(cx as i32 + x_soft_shake + para.shaky_coffee.4, cy as i32 + y_soft_shake + para.shaky_coffee.5, para.WIN_W, para.WIN_H);
+			
+			let i3 = i*3;
+			
+			buf[c] = 
+				graphical_fn::rgb_to_u32(
+                para.IMG.image_buffer[i3],
+                para.IMG.image_buffer[i3+1],
+                para.IMG.image_buffer[i3+2]
             );
-    }
+		}
+	}
 
-    _i = (_i + incr) % 1.0;
+    para.shaky_coffee.0 = (para.shaky_coffee.0 + incr) % 1.0;
 }
 
-static mut xshake : f32 = 0.0;
-static mut yshake : f32 = 0.0;
+//~ static mut xshake : f32 = 0.0;
+//~ static mut yshake : f32 = 0.0;
 
-static mut _j : f32 = 0.0;
+//~ static mut _j : f32 = 0.0;
 
-static mut smooth_amplitude : f32 = 0.0;
 const wrapper : f32 = crate::constants::pi2*725.0;
 
-pub unsafe fn apply_coord3(stream : &[(f32, f32)]) {
+pub fn apply_coord3(stream : &[(f32, f32)], para: &mut Parameters) {
 	
-    let mut data_f = vec![(0.0f32, 0.0f32); 1024];
+	let _j = &mut para.shaky_coffee.1;
+	let xshake = &mut para.shaky_coffee.2;
+	let yshake = &mut para.shaky_coffee.3;
+	let x_ = &mut para.shaky_coffee.4;
+	let y_ = &mut para.shaky_coffee.5;
+	
+    let mut data_f = [(0.0f32, 0.0f32); 1024];
     let mut amplitude : f32 = 0.0;
     
     for i in 0..1024 {
-        data_f[i].0 = (stream[i.min(stream.len())].0*VOL_SCL);
+        data_f[i].0 = (stream[i.min(stream.len())].0*para.VOL_SCL);
     }
     
     math::hanning(&mut data_f);
-    math::lowpass(&mut data_f, 0.005);
+    math::lowpass_array(&mut data_f, 0.005);
     
     for i in 0..1024 {
         amplitude += data_f[i].0.abs();
     }
     
     //smooth_amplitude = graphical_fn::linear_interp(smooth_amplitude, amplitude.min(1024.0), 0.5);
-    smooth_amplitude = (amplitude.clamp(1.0, 1024.0).log2()/1.9).powi(2);
+    let smooth_amplitude = (amplitude.clamp(1.0, 1024.0).log2()/1.9).powi(2);
     //println!("{}", smooth_amplitude);
     
-    _j = (_j + amplitude / 240.0) % wrapper;
+    *_j = (*_j + amplitude / 240.0) % wrapper;
     
-    xshake = (smooth_amplitude)*(_j).cos();
-    yshake = (smooth_amplitude)*(_j*0.725).sin();
+    *xshake = (smooth_amplitude)*(*_j).cos();
+    *yshake = (smooth_amplitude)*(*_j*0.725).sin();
     
-    x_ = graphical_fn::linear_interp(x_ as f32, xshake, 0.1) as i32;
-    y_ = graphical_fn::linear_interp(y_ as f32, yshake, 0.1) as i32;
+    *x_ = graphical_fn::linear_interp((*x_) as f32, (*xshake), 0.1) as i32;
+    *y_ = graphical_fn::linear_interp((*y_) as f32, (*yshake), 0.1) as i32;
     
-    _j += 0.01;
+    *_j += 0.01;
 }
