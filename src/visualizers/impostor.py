7a374eb4ffdea53ad981228dda53a01d18c0a820
khoidauminh <casboi86@gmail.com>
2025-03-07 23:25:44 +0700

    Polish python code

--- src/visualizers/impostor.py
+++ src/visualizers/impostor.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-from collections import deque
 from ast import arg
 import time
 import struct
 import os
 import math
 
+# Paths to the coffeevis files.
 audio_path = '/dev/shm/coffeevis_audio.bin'
 program_path = '/dev/shm/coffeevis_program.txt'
 commadn_path = '/dev/shm/coffeevis_command.txt'
@@ -15,42 +15,45 @@ file_audio = open(audio_path, mode='rb')
 file_commands = open(commadn_path, mode='w')
 file_program = open(program_path, mode='r')
 
+# We track for modified time and read when it's newer, instead of reading
+# the contents every time.
 audio_modified_time = 0.0
 program_modified_time = 0.0
 
-smooth = 0.0
-index = 0
-rotate_size = 300
+# This is similar to AudioBuffer in coffeeivs. Audio data only updates
+# a few times per second, so we only use a chunk of it, then rotate the
+# array to get the next chunk. This also records the number of rotations
+# happened and set rotate_size to the average, smoothening the updates.
 rotate_times = 0
+rotate_size = 300
 
+# Default program info
+# Will be updated in the event loop.
 program_w = 50
 program_h = 50
 duration = 1 / 72
 
-def read_to_array(f) -> tuple[list[float], list[float]]:
+def read_to_array(f) -> list[tuple[float, float]]:
     incr = 8
 
     file_audio.seek(0)
     fileContent = file_audio.read()
 
     length = len(fileContent)
-    arrayleft = []
-    arrayright = []
+    sample_array = []
 
     for i in range(0, length, incr):
         pair = struct.unpack("@ff", fileContent[i:i+incr])
-        arrayleft.append(pair[0])
-        arrayright.append(pair[1])
+        sample_array.append(pair)
 
-    return (arrayleft, arrayright)
+    return sample_array
 
-arrayleft, arrayright = read_to_array(file_audio)
-
+sample_array = read_to_array(file_audio)
 
 while True:
-
     mtime_prog = os.path.getmtime(program_path)
 
+    # Checks if program txt file was updated.
     if program_modified_time != mtime_prog:
         program_modified_time = mtime_prog
         file_program.seek(0)
@@ -63,36 +66,39 @@ while True:
 
         program_w = int(file_program_contents[1])
         program_h = int(file_program_contents[2])
-        refresh_rate = int(file_program_contents[3]) / 1000
+        refresh_rate = int(file_program_contents[3])
         duration = 1 / refresh_rate
 
     mtime_audio = os.path.getmtime(audio_path)
 
     if audio_modified_time != mtime_audio:
         audio_modified_time = mtime_audio
-        arrayleft, arrayright = read_to_array(file_audio)
-        rotate_size = len(arrayleft) // (rotate_times+1)
+
+        sample_array = read_to_array(file_audio)
+
+        # Computes ideal rotate_size
+        rotate_size = len(sample_array) // (rotate_times + 1)
         rotate_times = 0
     else:
         rotate_times += 1
-        arrayleft = arrayleft[rotate_size:] + arrayleft[:rotate_size]
-        arrayright = arrayright[rotate_size:] + arrayright[:rotate_size]
+        sample_array = sample_array[rotate_size:] + sample_array[:rotate_size]
 
-    arrl = arrayleft[:200]
-    arrr = arrayright[:200]
-    length = len(arrl)
+    length = len(sample_array) // 3
 
     file_commands.seek(0)
 
-    for i in range(len(arrl)):
-        xl = int(arrl[i]*program_h//2 + program_h//2)
-        xr = int(arrr[i]*program_h//2 + program_h//2)
+    # Prefer to write everything to file at once
+    string_out = ''
+
+    for i in range(length):
+        xl = int(sample_array[i][0]*program_h / 2 + program_h /2)
+        xr = int(sample_array[i][1]*program_h / 2 + program_h / 2)
         r = int(i * 255 // length)
         gb = 255 - r
-        file_commands.write("C FF {:02x} FF {:02x} o p {:04x} {:04x}\n".format(r, gb, i * program_w // length, xl))
-        file_commands.write("C FF {:02x} {:02x} FF o p {:04x} {:04x}\n".format(r, gb, i * program_w // length, xr))
+        string_out += "C FF {:02x} FF {:02x} o p {:04x} {:04x}\n".format(r, gb, i * program_w // length, xl)
+        string_out += "C FF {:02x} {:02x} FF o p {:04x} {:04x}\n".format(r, gb, i * program_w // length, xr)
 
-
+    file_commands.write(string_out)
     file_commands.flush()
 
     time.sleep(duration)
